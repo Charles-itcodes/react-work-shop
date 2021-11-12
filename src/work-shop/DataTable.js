1f9f8d7594d5916a90eeb3be370d7efaf306b1fe
Charles <rudahushac@gmail.com>
2021-11-12 11:18:32 +0100

    Show Details Student List Done.

--- src/work-shop/DataTable.js
+++ src/work-shop/DataTable.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 const DataTable = () => {
     const initialData =[{id:1,firstName:'Charles',lastName:'Rudahusha',age:30,birthDate:'1990-01-20',country:'Sweden',city:'Varnamo'},
-                       {id:2,firstName:'James',lastName:'Rurangwa',age:28,birthDate:'1993-01-20',country:'Sweden',city:'Varnamo'}];
+                       {id:2,firstName:'Egidia',lastName:'Uwera',age:21,birthDate:'2000-01-01',country:'Sweden',city:'Varnamo'},
+                       {id:3,firstName:'James',lastName:'Rurangwa',age:28,birthDate:'1992-05-10',country:'Sweden',city:'Varnamo'}];
 
     const [studentList, setStudentList]= useState(initialData);
+    const [showDetails, setShowDetails]= useState(false);
+    const [student, setStudent]= useState({id:0,firstName:"", lastName:"", age:0,birthDate:"",country:"",city:""});
 
     const TableHeader=()=>{
         return(<thead>
@@ -12,6 +15,9 @@ const DataTable = () => {
             <td>FirstName</td>
             <td>LastName</td>
             <td>Age</td>
+            <td>BirthDate</td>
+            <td>Country</td>
+            <td>City</td>
             <td>Action</td>
         </thead>
         );
@@ -22,14 +28,37 @@ const TableRow=(props)=>{
         <tbody>
             {
                 props.list.map((student)=>(
-                <tr>
+                <tr key={student.id}> 
                     <td>{student.id}</td>
                     <td>{student.firstName}</td>
                     <td>{student.lastName}</td>
                     <td>{student.age}</td>
+                    <td>{student.birthDate}</td>
+                    <td>{student.country}</td>
+                    <td>{student.city}</td>
+                    <td><TableAction student={student}/></td>
                     </tr>))
             }
         </tbody>
+        );
+    };
+
+    const TableAction=(props)=>{
+        const showData=()=>{
+            setShowDetails(true);
+console.log("SHOW DATA", props.student);
+setStudent(props.student);
+        };
+        return(<button type="button" className="btn btn-primary" onClick={showData}>Details</button>);
+
+    };
+    const ShowStudentDetails=()=>{
+        if (ShowStudentDetails){}
+
+        return(
+            <div className="card">
+                <div className="card-header bg-info text-white">Student information</div>
+            </div>
         );
     };
 
@@ -39,6 +68,19 @@ const TableRow=(props)=>{
               <TableHeader/>
               <TableRow list={studentList}/>
           </table>
+          <br/>
+          <ShowStudentDetails/>
+          <div className="card-body">
+              <h5 className="card-tittle">Country and City</h5>
+              <p className="card-text">ID:{student.id}</p>
+              <p className="card-text">Name:{student.firstName} {student.lastName}</p>
+              <p className="card-text">BirthDate:{student.birthDate}</p>
+              <p className="card-text">Country:{student.country}</p>
+              <p className="card-text">City:{student.city}</p>
+              
+
+
+          </div>
         </div>
     );
 };
